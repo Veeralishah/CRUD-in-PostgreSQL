183351ebfa27e2790956c5d7ea275e8c94ec9da2
Veerali Shah <veerali.shah@drcsystems.com>
2017-07-03 12:04:18 +0530

    config file commit

---
+++ db_config.py
@@ -0,0 +1,27 @@
+#-*- coding: utf8 -*-
+
+import psycopg2 as pdb
+
+
+# CREATE THE DATABASE
+
+def creatdb_postgres():
+    con = pdb.connect(database="testdb", user="postgres",
+                  password="Drc@1234", host="127.0.0.1", port="5432")
+    print "Create database testdb"
+
+# SET UP THE CONNECTION
+try:
+    con = pdb.connect(database="testdb", user="postgres",
+                      password="Drc@1234", host="127.0.0.1", port="5432")
+    cur = con.cursor()
+    cur.execute('SELECT version()')
+    ver = cur.fetchone()
+    print ver
+
+except pdb.DatabaseError, e:
+
+    print "Error %d: %s" % (e.args[0], e.args[1])
+    sys.exit(1)
+
+creatdb_postgres()
